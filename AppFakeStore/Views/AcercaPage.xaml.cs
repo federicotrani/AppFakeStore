8f226eafd8d6bdaa2465143ceff4878d5eac6c21
Federico Trani <federicotrani@gmail.com>
2024-07-30 16:20:33 -0300

    Ajuste pantall acerca

--- AppFakeStore/Views/AcercaPage.xaml.cs
+++ AppFakeStore/Views/AcercaPage.xaml.cs
@@ -5,15 +5,15 @@ public partial class AcercaPage : ContentPage
 	public AcercaPage()
 	{
 		InitializeComponent();
-	}
-
-    private void OnCounterClicked(object sender, TappedEventArgs e)
-    {
-		Application.Current.MainPage.DisplayAlert("Aviso", "Mostramos mensaje!", "Aceptar");
-    }
+	}    
 
     private void IconBtn_Clicked(object sender, EventArgs e)
     {
         Application.Current.MainPage.DisplayAlert("Aviso", "Mostramos mensaje Alternativo!", "Aceptar");
+    }
+
+    private void BackBtn_Clicked(object sender, EventArgs e)
+    {
+        Application.Current.MainPage.Navigation.PopAsync();
     }
 }
